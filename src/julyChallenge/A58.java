3b86b88fbd38e3f0dad28693b52f457636c1251b
Fattijenishbek <fattijenishbek01@.com>
2020-09-04 11:47:44 +0600

    challenge

--- src/julyChallenge/A58.java
+++ src/julyChallenge/A58.java
@@ -1,40 +1,56 @@
 package julyChallenge;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Scanner;
 
 public class A58 {
-    public static void main(String [] args){
+    public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
-        String str ="helo";
-        String input = scanner.nextLine();
-        List<String> arr = new ArrayList<>();
-        int h = input.indexOf("h");
-        String word = input.substring(h);
+        String input1 = scanner.nextLine();
+        if (input1.length() < 5) System.out.println("NO");
+        else {
+            int h1 = input1.indexOf("h");
+            String input = input1.substring(h1);
+            System.out.println(input);
+            int h = input.indexOf("h");
+            int e = input.indexOf("e");
+            int l1 = input.indexOf("l");
+            int l2 = input.indexOf("l", l1+1 );
+            int o = input.indexOf("o");
+            if (h > e) {
+                while (h > e) {
+                    e = input.indexOf("e", e );
+                    e++;
+                }
+            }
 
-        for (int i = 0; i <word.length() ; i++) {
-            arr.add(word.substring(i,i+1));
+            if (e > l1) {
+                while (e > l1) {
+                    l1 = input.indexOf("l", l1 );
+                    System.out.println(l1);
+                    l1++;
+                }
+            }
+            System.out.println(l1);
+
+            if (l1 > l2) {
+                while (l1 > l2) {
+                    l2 = input.indexOf("l", l1 + 1);
+                    l2++;
+                }
+            }
+            //System.out.println(l2);
+
+            if (l2 > o) {
+                while (l2 > o) {
+                    o = input.indexOf("o", o );
+                    o++;
+                }
+            }
+            System.out.println(h+" "+e+" "+l1+" "+l2+" "+o);
+
+            if (h < e && e <l1 && l1 < l2 && l2 < o) System.out.println("YES");
+            else System.out.println("NO");
+
         }
-        HashSet<String> set = new HashSet<>(arr);
-        System.out.println(arr.toString());
-
-        for (int i = 0; i <arr.size() ; i++) {
-            if (str.indexOf(arr.get(i))==-1) arr.remove(i);
-        }
-        System.out.println(arr.toString());
-
-        int count = 0;
-
-
-
-        for (int i = 0; i <4 ; i++) {
-            if (input.indexOf(str.charAt(i))!=-1) count++;
-        }
-        if(input.indexOf("l", input.indexOf("l")+1)!=-1) count++;
-
-        if (input.length()>5 && count==5) System.out.println("YES");
-        else System.out.println("NO");
     }
 }
